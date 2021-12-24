ed6555e7aed9300ed6bbadb73b340b472b07ea5f
senad velagic <velagicsenad@outlook.com>
2021-12-24 22:30:41 +0100

    my first commit

---
+++ src/components/Data.js
@@ -0,0 +1,26 @@
+export const Data = [
+  {
+    id: 1,
+    image: "realrealmadrid.jpg",
+    name: "Real Madrid (Navas)",
+    price: 45,
+  },
+  {
+    id: 2,
+    image: "madridrealmadrid.jpg",
+    name: "Real Madrid (Ronaldo)",
+    price: 100,
+  },
+  {
+    id: 3,
+    image: "realmadridreal.jpg",
+    name: "Real Madrid (Isco)",
+    price: 200,
+  },
+  {
+    id: 4,
+    image: "realmadrid.jpg",
+    name: "Real Madrid(Benzema)",
+    price: 120,
+  },
+];
---
+++ src/components/Products.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Data } from "./Data";
+import { GiShoppingBag } from "react-icons/gi";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+const Products = () => {
+  const cart = useSelector((state) => state);
+  console.log(cart);
+  const dispatch = useDispatch();
+  return (
+    <div className="productConatiner">
+      <div className="navigation">
+        
+        <p>
+          <HiOutlineMenuAlt4 />
+        </p>
+        <h3>Real Madrid Shop</h3>
+        <Link to="/cart">
+          <GiShoppingBag />
+        </Link>
+      </div>
+      <div className="products">
+        {Data.map((item) => {
+          item.quantity = 1;
+          return (
+            <div className="product" key={item.id}>
+              <img src={` ../images/${item.image}`} alt="cart" />
+              <h4>{item.name}</h4>
+              <p>£. {item.price}</p>
+              <button onClick={(alert) => dispatch({ type: "ADD", payload: item })}>
+                add to cart
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
